3b9890888efdf61785f3ea3eabb994d17937d8fd
Lara Marie Reimer <lara.reimer@tum.de>
2016-01-22 21:40:58 +0100

    Added default xCode project

---
+++ StudyOrganizer/StudyOrganizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudyOrganizer
+//
+//  Created by Lara Marie Reimer on 22.01.16.
+//  Copyright © 2016 Lara Marie Reimer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
